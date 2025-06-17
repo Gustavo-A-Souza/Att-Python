4bfdd2df168a666d218e290b61ceee356e64b90b
Gustavo-A-Souza <gustavoasouza057@gmail.com>
2025-06-17 17:36:16 -0300

    Update Etapa1 _ 7.py

--- Etapa1 _ 7.py
+++ Etapa1 _ 7.py
@@ -4,10 +4,17 @@ SM2 = float(input("Digite a SM2: "))
 AV = float(input("Digite a AV: "))
 
 media = (SM1 + SM2 + AV)
+media_F = media
 
 if media >= 6.0:
-    media_F = media
     status = "Aprovado"
 else:
-    status = "Re-provado"
-print(f"A média final do {Nome} foi {media_F:.1f} tendo sido {status}.")
+    print(f"A média do {Nome} foi  {media:.1f}, é preciso fazer recuperação.")
+    NT = float(input("Digite a NT: "))
+    media_F = NT
+    if media_F >= 6.0:
+        status = "Aprovado"
+    else:
+        status = "Reprovado"
+
+print(f"A média final do {Nome} foi {media_F:.1f} tendo sido {status}.
