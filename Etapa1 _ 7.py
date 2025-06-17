246580b76425b03a5e3e464150473181447d48fd
Gustavo-A-Souza <gustavoasouza057@gmail.com>
2025-06-17 17:50:56 -0300

    Update Etapa1 _ 7.py

--- Etapa1 _ 7.py
+++ Etapa1 _ 7.py
@@ -2,19 +2,13 @@ Nome = input("Digite seu nome: ")
 SM1 = float(input("Digite a SM1: "))
 SM2 = float(input("Digite a SM2: "))
 AV = float(input("Digite a AV: "))
+NT = float(input("Digite a NT: "))
+    
+media_Inicial = (SM1 + SM2 + AV)*0.7 + NT*0.3
 
-media = (SM1 + SM2 + AV)
-media_F = media
-
-if media >= 6.0:
+if media_Inicial >= 6.0:
     status = "Aprovado"
 else:
-    print(f"A média do {Nome} foi  {media:.1f}, é preciso fazer recuperação.")
-    NT = float(input("Digite a NT: "))
-    media_F = NT
-    if media_F >= 6.0:
-        status = "Aprovado"
-    else:
-        status = "Reprovado"
+    status = "Reprovado"
 
-print(f"A média final do {Nome} foi {media_F:.1f} tendo sido {status}.
+print(f"A média final do {Nome} foi {media_Inicial:.1f} tendo sido {status}.")
